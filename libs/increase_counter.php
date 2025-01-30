0dd0e4fa6b01ff536cd4dfdab625a291b2eacf01
agahakbatur <agahakbatur@gmail.com>
2025-01-30 21:31:35 +0300

    Update increase_counter.php

--- libs/increase_counter.php
+++ libs/increase_counter.php
@@ -2,7 +2,7 @@
   // $counter_name = 'Counter.txt';
   echo "This is Button1 that is selected";
   echo '<script>alert("Welcome to Geeks for Geeks")</script>';
-  $counter_name = 'file:///http://127.0.0.1:5500/libs/counter.txt';
+  $counter_name = 'file:///https://agahakbatur.github.io/ogreticiler/libs/counter.txt';
 
   if (!file_exists($counter_name)) {
     $f = fopen($counter_name, "w");
